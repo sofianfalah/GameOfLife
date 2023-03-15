aad90fb05e03fe142434e2ef64465ed2a91c54a4
sofianfalah <94194077+sofianfalah@users.noreply.github.com>
2023-03-15 20:22:38 +0200

    Add files via upload

---
+++ Semaphore.cpp
@@ -0,0 +1,30 @@
+
+#include "Semaphore.hpp"
+
+Semaphore::Semaphore() {
+    counter=0;
+    pthread_cond_init(&cond,NULL);
+    pthread_mutex_init(&mutex,NULL);
+}
+
+Semaphore::Semaphore(unsigned int val) {
+    counter=val;
+    pthread_cond_init(&cond,NULL);
+    pthread_mutex_init(&mutex,NULL);
+}
+
+void Semaphore::up() {
+    pthread_mutex_lock(&(mutex));
+    counter++;
+    pthread_cond_broadcast(&(cond)); 
+    pthread_mutex_unlock(&(mutex));
+}
+
+void Semaphore::down() {
+    pthread_mutex_lock(&(mutex));
+    while (counter == 0) {
+        pthread_cond_wait(&(cond),&(mutex));
+    }
+    counter--;
+    pthread_mutex_unlock(&(mutex));
+}
